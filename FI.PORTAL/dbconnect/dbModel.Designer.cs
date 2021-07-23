67c7052c91d70550a888e3dc1698198e22833cfb
Manoj <manojm@nipsea.com.sg>
2021-07-23 23:46:10 +0530

    updates

--- FI.PORTAL/dbconnect/dbModel.Designer.cs
+++ FI.PORTAL/dbconnect/dbModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\manojm\source\repos\FI_Portal\FI.PORTAL\FI.PORTAL\dbconnect\dbModel.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\manojm\source\repos\NPLK.FI.PROJECT\FI.WEB\NPLK.FI.WEB.APP\FI.PORTAL\dbconnect\dbModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
